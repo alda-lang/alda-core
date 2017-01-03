23913c6af24e89c38bf763a215a884b6c254d97c
James Kiesel <james.kiesel@gmail.com>
2017-01-04 10:31:11 +1300

    Test each individual event-to-state application works

--- src/alda/import/midi.clj
+++ src/alda/import/midi.clj
@@ -1,59 +1,59 @@
 (ns alda.import.midi
-  (:import [javax.sound.midi MidiSystem]
+  (:import [javax.sound.midi MidiSystem ShortMessage]
            [java.io File])
 )
-
-(def note-on 144)
-(def note-off 128)
-(defstruct note :channel :pitch :volume :start :duration)
 
 (comment
   "This module accepts raw bytecode from a midi file, and converts it to alda syntax
    which is then printed to STDOUT.")
 
-(defn- begin-note
-  "Constructs the beginning of a vote given a NOTE_ON event"
-  [note-on-event]
-  (struct note
-    (-> note-on-event .getMessage .getChannel)
-    (-> note-on-event .getMessage .getData1)
-    (-> note-on-event .getMessage .getData2)
-    (-> note-on-event .getTick))
+(defn- note-on
+  "We've just heard a new note beginning, update state to include it"
+  [state event]
+  (assoc state :notes (concat (get state :notes) [(hash-map
+    :channel    (-> event .getMessage .getChannel)
+    :pitch      (-> event .getMessage .getData1)
+    :volume     (-> event .getMessage .getData2)
+    :start      (-> event .getTick)
+    :instrument (get state :instrument))]))
 )
 
-(defn- finish-note
-  "Adds the duration to an existing note given a NOTE_OFF event.
-   We do this by subtracting the note-off timestamp from the note-off timestamp"
-  [note-off-event half-note]
-  (assoc half-note :duration
-    (- (.getTick note-off-event) (get half-note :start)))
+(defn- note-off
+  "We've just heard a note complete; update state to include its duration"
+  [state event]
+  state ; TODO
 )
 
-(defn- note-from-event
-  "Takes a new event from the Track and convert it to a Note struct"
-  [event]
-  (let [message (.getMessage event)]
-    (cond
-      (not= (type message) com.sun.media.sound.FastShortMessage) nil
-      (= (.getCommand message) note-on)  (begin-note event)
-      (= (.getCommand message) note-off) (finish-note event (begin-note event))
-    ))
+(defn- program-change
+  "We've just switched instruments in this channel; update state to reflect this"
+  [state event]
+  (assoc state :instrument (-> event .getMessage .getData1))
+)
+
+(defn process-note-event
+  [state event]
+  (if (= (type (.getMessage event)) ShortMessage)
+    (case (-> event .getMessage .getCommand)
+      144 (note-on state event)
+      128 (note-off state event)
+      192 (program-change state event)
+      state)
+    state) ; this isn't an event we care about; return the original state
 )
 
 (defn- notes-from-track
-  "Read a track and get a series of note data from it"
+  "Get a final state by reading through a series of note events"
   [track]
-  (remove nil?
-    (for [tick (range 0 (.size track))]
-      (note-from-event (.get track tick))))
+  (get
+    (reduce process-note-event {}
+      (map #(.get track %)
+        (range 0 (.size track)))) :notes [])
 )
 
 (defn import-midi
   "Imports a .mid or .midi file specified by path, and prints it to STDOUT"
   [path]
-  (remove empty?
+  (flatten
     (map notes-from-track
-      (.getTracks
-        (MidiSystem/getSequence
-          (new File path)))))
+      (-> (new File path) MidiSystem/getSequence .getTracks)))
 )
--- test/alda/import/midi_test.clj
+++ test/alda/import/midi_test.clj
@@ -1,10 +1,48 @@
 (ns alda.import.midi-test
   (:require [clojure.test      :refer :all]
             [alda.test-helpers :refer (get-instrument)]
-            [alda.import.midi  :refer :all]))
+            [alda.import.midi  :refer :all])
+  (:import [javax.sound.midi MidiEvent MidiMessage ShortMessage]
+           [com.sun.media.sound FastShortMessage])
+)
+
+(defn- build-event
+  "Build an event for testing"
+  ([command]                     (build-event command 0       0     0     10))
+  ([command channel]             (build-event command channel 0     0     10))
+  ([command channel data1]       (build-event command channel data1 0     10))
+  ([command channel data1 data2] (build-event command channel data1 data2 10))
+  ([command channel data1 data2 seq]
+    (new MidiEvent (new ShortMessage command channel data1 data2) seq))
+)
 
 (deftest midi-tests
-  (testing "we can chain function calls together"
+  (testing "can process a midi file"
     (is (= "some test data" (import-midi "./examples/midi/twotone.mid")))
+  )
+
+  (testing "note-on adds a new note"
+    (let [event (build-event 144)
+          state (process-note-event {} event)]
+      (is (= 1 (count (get state :notes)))))
+  )
+
+  (testing "note-off updates an existing note with the duration"
+    (let [event (build-event 128)
+          prev-state (process-note-event {} (build-event 144))
+          state (process-note-event prev-state event)]
+      (is (not= nil (get-in state [:notes 0 :duration]))))
+  )
+
+  (testing "change-program updates the instrument of the state"
+    (let [event (build-event 192 0 15) ; include '15' as the data for the instrument to change to
+          state (process-note-event {} event)]
+      (is (= 15 (get state :instrument))))
+  )
+
+  (testing "other events don't change the state"
+    (let [event (build-event 208)
+          state (process-note-event {} event)]
+      (is (= {} state)))
   )
 )
