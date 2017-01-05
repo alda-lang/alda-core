e6765f4658ace0ba951b37779ddbe2f4fc7f7c16
James Kiesel <james.kiesel@gmail.com>
2017-01-05 17:31:42 +1300

    Now allows imports of basic MIDI files to data array

--- src/alda/import/midi.clj
+++ src/alda/import/midi.clj
@@ -1,5 +1,5 @@
 (ns alda.import.midi
-  (:import [javax.sound.midi MidiSystem ShortMessage]
+  (:import [javax.sound.midi MidiSystem]
            [java.io File])
 )
 
@@ -7,21 +7,36 @@
   "This module accepts raw bytecode from a midi file, and converts it to alda syntax
    which is then printed to STDOUT.")
 
+(defn- find-existing
+  "Given some criteria, find the first matching hash in a collection of hashes" ; I feel like clojure should be able to do this for me?
+  [collection criteria]
+  (some #(if (= criteria (select-keys % (keys criteria))) %) collection)
+)
+
 (defn- note-on
-  "We've just heard a new note beginning, update state to include it"
+  "We've just heard a new note beginning, update state partials to include it"
   [state event]
-  (assoc state :notes (concat (get state :notes) [(hash-map
+  (assoc state :partials (conj (get state :partials) (hash-map
     :channel    (-> event .getMessage .getChannel)
     :pitch      (-> event .getMessage .getData1)
     :volume     (-> event .getMessage .getData2)
     :start      (-> event .getTick)
-    :instrument (get state :instrument))]))
+    :instrument (get state :instrument)
+    :duration   nil))) ; duration will be set when the note completes
 )
 
 (defn- note-off
   "We've just heard a note complete; update state to include its duration"
   [state event]
-  state ; TODO
+  (let [partial (find-existing (get state :partials) {
+          :pitch    (-> event .getMessage .getData1)
+          :channel  (-> event .getMessage .getChannel)
+          :duration nil })]
+    (if (nil? partial)
+      state                                                                ; couldn't find corresponding note; return state as-is
+      (assoc state :partials (remove #(= % partial) (get state :partials)) ; remove partial note
+                   :notes    (conj (get state :notes)                      ; and re-add the completed note
+                               (assoc partial :duration (- (.getTick event) (get partial :start)))))))
 )
 
 (defn- program-change
@@ -30,15 +45,24 @@
   (assoc state :instrument (-> event .getMessage .getData1))
 )
 
+(defn- is-defined?
+  ; This is pretty ugly: We get an array of Events, some of which respond to
+  ; getCommand() and some don't... we need to filter out the ones which don't
+  ; otherwise the world blows up. Seeing if 'getCommand' was in the list of methods
+  ; on the Java class was the only way I could get to go, but there's got to be a better way.
+  "See if a java class responds to a method"
+  [message method]
+  (some #(= (.getName %) method) (-> message .getClass .getMethods))
+)
+
 (defn process-note-event
   [state event]
-  (if (= (type (.getMessage event)) ShortMessage)
-    (case (-> event .getMessage .getCommand)
+  (let [message (.getMessage event)]
+    (case (if (is-defined? message "getCommand") (.getCommand message))
       144 (note-on state event)
       128 (note-off state event)
       192 (program-change state event)
-      state)
-    state) ; this isn't an event we care about; return the original state
+      state)) ; this isn't an event we care about; return the original state
 )
 
 (defn- notes-from-track
@@ -46,8 +70,8 @@
   [track]
   (get
     (reduce process-note-event {}
-      (map #(.get track %)
-        (range 0 (.size track)))) :notes [])
+      (map #(.get track %) (range (.size track)))) ; fetch all events from the given track
+        :notes ())
 )
 
 (defn import-midi
--- test/alda/import/midi_test.clj
+++ test/alda/import/midi_test.clj
@@ -8,30 +8,56 @@
 
 (defn- build-event
   "Build an event for testing"
-  ([command]                     (build-event command 0       0     0     10))
-  ([command channel]             (build-event command channel 0     0     10))
-  ([command channel data1]       (build-event command channel data1 0     10))
+  ([command]                     (build-event command 0))
+  ([command channel]             (build-event command channel 0))
+  ([command channel data1]       (build-event command channel data1 0))
   ([command channel data1 data2] (build-event command channel data1 data2 10))
   ([command channel data1 data2 seq]
     (new MidiEvent (new ShortMessage command channel data1 data2) seq))
 )
 
 (deftest midi-tests
-  (testing "can process a midi file"
-    (is (= "some test data" (import-midi "./examples/midi/twotone.mid")))
+  (testing "can process a short midi file"
+    (let [result (import-midi "./examples/midi/twotone.mid")]
+      ; we get a collection with two tones
+      (is (= 2 (count result)))
+
+      ; first note
+      (is (= 0     (get (first result) :instrument)))
+      (is (= 0     (get (first result) :start)))
+      (is (= 0     (get (first result) :channel)))
+      (is (= 72    (get (first result) :pitch)))
+      (is (= 11520 (get (first result) :duration)))
+      (is (= 40    (get (first result) :volume)))
+      ; second note
+      (is (= 0     (get (last result) :instrument)))
+      (is (= 1440  (get (last result) :start)))
+      (is (= 0     (get (last result) :channel)))
+      (is (= 74    (get (last result) :pitch)))
+      (is (= 7200  (get (last result) :duration)))
+      (is (= 40    (get (last result) :volume)))
+    )
   )
 
-  (testing "note-on adds a new note"
-    (let [event (build-event 144)
+  ; this doesn't work yet, I think because this file has 0-duration NOTE_ON events in lieu of NOTE_OFF events
+  ; (apparantly that's a thing that MIDI does sometimes)
+  ; (testing "can process a big midi file"
+  ;   (is (= "some test data" (import-midi "./examples/midi/pokemon.mid")))
+  ; )
+
+  (testing "note-on adds a new note partial"
+    (let [event (build-event 144 9)
           state (process-note-event {} event)]
-      (is (= 1 (count (get state :notes)))))
+      (is (= 1 (count (get state :partials))))
+      (is (= 9 (get (first (get state :partials)) :channel))))
   )
 
   (testing "note-off updates an existing note with the duration"
-    (let [event (build-event 128)
-          prev-state (process-note-event {} (build-event 144))
+    (let [event (build-event 128 0 0 0 50)
+          prev-state (process-note-event {} (build-event 144 0 0 0 10))
           state (process-note-event prev-state event)]
-      (is (not= nil (get-in state [:notes 0 :duration]))))
+      (is (= 1 (count (get state :notes))))
+      (is (= 40 (get (first (get state :notes)) :duration))))
   )
 
   (testing "change-program updates the instrument of the state"
