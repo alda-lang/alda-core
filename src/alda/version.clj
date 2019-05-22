e27b1d07224b5951fca63277374e957ed399c8f5
Dave Yarwood <dave.yarwood@gmail.com>
2019-05-21 21:00:43 -0400

    add .clj-kondo/.cache/ to .gitignore; rm unused requires

--- src/alda/version.clj
+++ src/alda/version.clj
@@ -1,7 +1,5 @@
 (ns alda.version
-  (:require [clojure.java.io :as io]
-            [clojure.string  :as str]
-            [manifest.core   :refer (manifest)]))
+  (:require [manifest.core :refer (manifest)]))
 
 (def ^:const -version-
   (or (:alda-version (manifest "alda.Main"))
